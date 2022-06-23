49fbd85b638245bae5dedddcfcb304a8e95d43c5
Alexander Pino <alexanderpino@hotmail.com>
2022-06-22 23:41:37 -0500

    Commit inicial, lists and dicts anidados

---
+++ lists_and_dicts.py
@@ -0,0 +1,32 @@
+from multiprocessing.sharedctypes import Value
+
+
+def run():
+    my_list = [1, "Hello", True, 4.5]
+    my_dict = {"fristname": "Alexander", "lastname": "Pino"}
+
+    super_list = [
+        {"fristname": "Alexander", "lastname": "Pino"},
+        {"fristname": "Miguel", "lastname": "Torres"},
+        {"fristname": "Pepe", "lastname": "Rodelo"},
+        {"fristname": "Susana", "lastname": "Miranda"},
+        {"fristname": "Jose", "lastname": "Pino"},
+    ]
+
+    super_dict = {
+        "natural_nums": [1, 2, 3, 4, 5],
+        "integer_nums": [-1, -2, 0, 1, 2],
+        "floating_nums": [1.1, 4.5, 6.43]
+    }
+
+    for key, value in super_dict.items():
+        print(key, "-", value)
+
+    for i in super_list:
+        print(i["fristname"], i["lastname"])
+
+    
+
+
+if __name__ == '__main__':
+    run()
