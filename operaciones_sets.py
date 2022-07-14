814472b98361714c4b15233fcd237c64c77eee67
Alexander Pino <alexanderpino@hotmail.com>
2022-07-14 00:02:58 -0500

    Operation with sets

---
+++ operaciones_sets.py
@@ -0,0 +1,11 @@
+my_set1 = {3, 4, 5}
+my_set2 = {5, 6, 7}
+
+my_set3 = my_set1 | my_set2 # Union
+my_set4 = my_set1 & my_set2 # interseccion
+my_set5 = my_set1 - my_set2 # diferencia
+my_set6 = my_set2 - my_set1 # Diferencia
+my_set7 = my_set1 ^ my_set2 # Diferencia simetrica
+
+print(my_set7)
+
