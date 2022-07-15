4a7c642b447e958270924a0473802af6bacfa094
Alexander Pino <alexanderpino@hotmail.com>
2022-07-14 22:49:43 -0500

    sets and date and time zones

---
+++ sets_without_duplicates.py
@@ -0,0 +1,14 @@
+def sup_duplicates(random_list):
+    return list(set(random_list))
+
+def run():
+    random_list = [1, 1, 2, 2, 4, 3]
+    print(sup_duplicates(random_list))
+
+if __name__ == '__main__':
+    run()
+
+
+
+
+
---
+++ time_zone.py
@@ -0,0 +1,16 @@
+from datetime import datetime
+import pytz
+
+bogota_timezone = pytz.timezone("America/Bogota")
+bogota_date = datetime.now(bogota_timezone)
+print("Bogota: ", bogota_date.strftime("%d/%m/%Y, %H:%M:%S"))
+
+
+mexico_timezone = pytz.timezone("America/Mexico_City")
+mexico_date = datetime.now(mexico_timezone)
+print("Mexico: ", mexico_date.strftime("%d/%m/%Y, %H:%M:%S")) 
+
+
+caracas_timezone = pytz.timezone("America/Caracas")
+caracas_date = datetime.now(caracas_timezone)
+print("Caracas: ", caracas_date.strftime("%d/%m/%Y, %H:%M:%S")) 
