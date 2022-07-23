0ed30c2a7281be12a7e4da72b56bbdb84e0db180
Alexander Pino <alexanderpino@hotmail.com>
2022-07-23 00:11:50 -0500

    Pruebas para verificar pandas y numpu

--- numpy/copy.py
+++ numpy/copy.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 arr = np.arange(0,11)
 print(arr)
--- numpy/el_array.py
+++ numpy/el_array.py
@@ -4,7 +4,7 @@ import numpy as np
 lista = [1,2,3,4,5,6,7,8,9]    
 print(lista)
 
-arr = np.array(lista)    
+arr = np.array(lista)     
 print(type(arr))
 
 matriz = [[1,2,3],[4,5,6],[7,8,9]]
