a2a524e9a9504195f19b759d53ff774ee06c333b
Alexander Pino <alexanderpino@hotmail.com>
2022-07-27 22:43:31 -0500

    Solucionar el borrado del archivo copy, y mover series_dat.. a pandas

---
+++ pandas/series_dataframes.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+psg_players = pd.Series(['Nabas', 'Mbappe', 'Neymar', 'Messi'], index=[1,7,10,30])
+
+print(psg_players)
+
+psg_players = pd.Series(['Nabas', 'Mbappe', 'Neymar', 'Messi'])
+print(psg_players)
+
+
+
+
+
+
+
+
+
+
+
+
